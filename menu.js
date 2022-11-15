c5de775d16c123683974612752c49c76e6c553e6
admirerbrown <admirerbrown@gmail.com>
2022-11-15 11:32:36 +0000

    add direct click action to associate section

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector(".fa-bars").addEventListener("click",menuList);
+const hamburger = document.querySelector(".fa-bars").addEventListener("click", menuList);
 const menu = document.querySelector(".mark").addEventListener("click", toggleMenu);
 const openIcon = document.querySelector(".bars");
 const closeIcon = document.querySelector(".mark");
 const page = document.querySelectorAll(".list").length;
 
 function menuList() {
-     openIcon.style.display = "none";
-     closeIcon.style.display = "block";
-     document.querySelector(".main-content").style.visibility = "hidden";
-     document.querySelector(".logo").style.visibility = "hidden";
-     document.querySelector("#menu-list").style.display = "block";
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
+    document.querySelector(".main-content").style.visibility = "hidden";
+    document.querySelector(".logo").style.visibility = "hidden";
+    document.querySelector("#menu-list").style.display = "block";
 }
 
 function toggleMenu() {
@@ -20,4 +20,11 @@ function toggleMenu() {
     document.querySelector("#menu-list").style.display = "none";
 }
 
-
+for (let a = 0; a < page; a++) {
+    document.querySelectorAll(".list")[a].addEventListener("click", () => {
+        document.querySelector(".main-content").style.visibility = "visible";
+        document.querySelectorAll(".logo").style.visibility = "visible";
+        document.querySelector("#menu-list").style.display = "none";
+        openIcon.style.display = "block";
+        closeIcon.style.display = "none";
+    })}; 
