2f0440e78634dec9c08f36f41287e91afa720fd9
JacintaNgwu <jacintangwu9900@gmail.com>
2022-11-15 15:52:03 +0100

    fix linters error in js

--- menu.js
+++ menu.js
@@ -1,30 +1,31 @@
-const hamburger = document.querySelector(".fa-bars").addEventListener("click", menuList);
-const menu = document.querySelector(".mark").addEventListener("click", toggleMenu);
-const openIcon = document.querySelector(".bars");
-const closeIcon = document.querySelector(".mark");
-const page = document.querySelectorAll(".list").length;
+const openIcon = document.querySelector('.bars');
+const closeIcon = document.querySelector('.mark');
+const page = document.querySelectorAll('.desktop-menu').length;
 
 function menuList() {
-     openIcon.style.display = "none";
-     closeIcon.style.display = "block";
-     document.querySelector(".main-content").style.visibility = "hidden";
-     document.querySelector(".logo").style.visibility = "hidden";
-     document.querySelector("#menu-list").style.display = "block";
+  openIcon.style.display = 'none';
+  closeIcon.style.display = 'block';
+  document.querySelector('.main-content').style.visibility = 'hidden';
+  document.querySelector('.logo').style.visibility = 'hidden';
+  document.querySelector('#menu-list').style.display = 'block';
 }
+document.querySelector('.fa-bars').addEventListener('click', menuList);
 
 function toggleMenu() {
-    openIcon.style.display = "block";
-    closeIcon.style.display = "none";
-    document.querySelector(".main-content").style.visibility = "visible";
-    document.querySelector(".logo").style.visibility = "visible";
-    document.querySelector("#menu-list").style.display = "none";
+  openIcon.style.display = 'block';
+  closeIcon.style.display = 'none';
+  document.querySelector('.main-content').style.visibility = 'visible';
+  document.querySelector('.logo').style.visibility = 'visible';
+  document.querySelector('#menu-list').style.display = 'none';
 }
+document.querySelector('.mark').addEventListener('click', toggleMenu);
 
-for (let a = 0; a < page; a++) {
-    document.querySelectorAll(".list")[a].addEventListener("click", () => {
-        document.querySelector(".main-content").style.visibility = "visible";
-        document.querySelectorAll(".logo").style.visibility = "visible";
-        document.querySelector("#menu-list").style.display = "none";
-        openIcon.style.display = "block";
-        closeIcon.style.display = "none";
-    })}; 
+for (let a = 0; a < page; a += 1) {
+  document.querySelectorAll('.desktop-menu')[a].addEventListener('click', () => {
+    document.querySelector('.main-content').style.visibility = 'visible';
+    document.querySelector('.logo').style.visibility = 'visible';
+    document.querySelector('#menu-list').style.display = 'none';
+    openIcon.style.display = 'block';
+    closeIcon.style.display = 'none';
+  });
+}
