13da741c3b0daf07280bdec775932522f0ab233b
JacintaNgwu <jacintangwu9900@gmail.com>
2022-11-15 12:19:13 +0100

    Update style.css file

--- menu.js
+++ menu.js
@@ -1,24 +1,23 @@
-alert("welcome");
+const hamburger = document.querySelector(".fa-bars").addEventListener("click",menuList);
+const menu = document.querySelector(".mark").addEventListener("click", toggleMenu);
+const openIcon = document.querySelector(".bars");
+const closeIcon = document.querySelector(".mark");
+const page = document.querySelectorAll(".list").length;
 
-const hamburger = document.querySelector(".hamburger").addEventListener("click",menuList);
-const menu = document.getElementById("menu");
-const icon = document.querySelector(".hamburger i");
-
-function myFunction() {
-    const x = document.getElementById("menu-list");
-    if(x.style.display === "block"){
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+function menuList() {
+     openIcon.style.display = "none";
+     closeIcon.style.display = "block";
+     document.querySelector(".main-content").style.visibility = "hidden";
+     document.querySelector(".logo").style.visibility = "hidden";
+     document.querySelector("#menu-list").style.display = "block";
 }
 
-function menuList(){
-    alert("clicked");
-    icon.classList.toggle("fa-xmark");
-    icon.classList.toggle("big-icon");
-    menu.classList.toggle("show");
+function toggleMenu() {
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
+    document.querySelector(".main-content").style.visibility = "visible";
+    document.querySelector(".logo").style.visibility = "visible";
+    document.querySelector("#menu-list").style.display = "none";
 }
-
 
 
